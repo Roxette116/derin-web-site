571aec860de393b8d572c4aa76c171817c7af3d8
Roxette116 <01denemehesap3334@gmail.com>
2026-09-23 23:58:56 +0300

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js';
+import { getAuth, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js';
+import { firebaseConfig } from './firebase-config.js';
+
+export const firebaseConfigured = Boolean(
+  firebaseConfig.apiKey &&
+  firebaseConfig.projectId &&
+  firebaseConfig.appId &&
+  !firebaseConfig.apiKey.startsWith('YOUR_') &&
+  !firebaseConfig.projectId.startsWith('YOUR_') &&
+  !firebaseConfig.appId.startsWith('YOUR_')
+);
+
+export let auth = null;
+export let googleProvider = null;
+export let db = null;
+
+if (firebaseConfigured) {
+  const app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({ prompt: 'select_account' });
+  db = getFirestore(app);
+}
